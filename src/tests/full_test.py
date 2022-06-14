5283f520981ef2fcb571ca1764e2ade8803bb640
martin <martinrovang@gmail.com>
2022-06-14 15:49:14 +0200

    added more tests

--- src/tests/full_test.py
+++ src/tests/full_test.py
@@ -1,9 +1,27 @@
 import pytest
 
-def test_dataintensities():
+def test_basic():
     """
     Test that the data intensities are correct.
     """
     with open('data/dataaa.txt') as f:
         value = f.read()
     assert int(value) > 0
+
+
+class TestClass:
+    def test_otherintensities(self):
+        """
+        Test that the data intensities are correct.
+        """
+        with open('data/dataaa.txt') as f:
+            value = f.read()
+        assert int(value) < 0
+    
+    def test_otherintensities2(self):
+        """
+        Test that the data intensities are correct.
+        """
+        with open('data/dataaa.txt') as f:
+            value = f.read()
+        assert int(value) > 0
